18fc282c0895a8b4442d6f446eee335d2b8ca8e9
root <root@localhost.localdomain>
2018-10-14 12:21:49 +0530

    reactive form set up

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  registrationForm = new FormGroup({
+    fullName: new FormControl(''),
+    sex: new FormControl(''),
+    birthDate: new FormControl(''),
+    email: new FormControl(''),
+    mobile: new FormControl(''),
+    continent: new FormControl(''),
+    skills: new FormControl(''),
+    photo: new FormControl(''),
+  });
 }
